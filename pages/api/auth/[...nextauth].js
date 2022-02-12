38db8c6b570afad448946e90e6e850d83ab14eaa
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-02-12 03:55:31 +0100

    feat:configuring

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,68 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+// import { refreshAccessToken } from "spotify-web-api-node/src/server-methods";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS", refreshedToken);
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshToken.expires_at,
+    };
+  } catch (error) {
+    console.error;
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
 
   // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      //initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000, //we are handling expiry times in miliseconds hence = 1000
+        };
+      }
+      //Return previous token if the token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("EXISTING ACCESS TOKEN IS VALID");
+        return token;
+      }
+      //Access token expires,we need to refresh it...
+      console.log("ACCESS TOKEN HAS EXPIRED,REFRESHING...");
+      return await refreshAccessToken(token);
+    },
+  },
 });
+
+<div></div>;
